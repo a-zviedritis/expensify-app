3a948928078c6da6e382f21a92c476dc4c7deb97
Andris <a.zviedritis@gmail.com>
2019-02-02 20:46:47 +0200

    Styling Expensify

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => (
-  <div>
-    {
-      expenseCount === 0 ? (
-        <p>No expenses to view</p>
-      ) : (
-        <p>Viewing {expenseCount} expense{expenseCount > 1 ? 's': ''} totalling €{numeral(expensesTotal / 100).format('0,0.00')}</p>
-      )
-    }
-  </div>
-);
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense': 'expenses';
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('0,0.00');
+
+  return (
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>€{formattedExpensesTotal}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
