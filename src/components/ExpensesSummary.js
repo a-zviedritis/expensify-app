66ed70a6a8c3c10a7c0e35597cbbae8fa0622e64
Andris <a.zviedritis@gmail.com>
2019-02-03 14:37:24 +0200

    Message informing about filtered out expenses

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,14 +5,16 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-  const expenseWord = expenseCount === 1 ? 'expense': 'expenses';
-  const formattedExpensesTotal = numeral(expensesTotal / 100).format('0,0.00');
+export const ExpensesSummary = ({ hiddenExpenseCount, visibleExpenseCount, visibleExpensesTotal }) => {
+  const hiddenExpenseWord = hiddenExpenseCount === 1 ? 'expense': 'expenses';
+  const visibleExpenseWord = visibleExpenseCount === 1 ? 'expense': 'expenses';
+  const formattedExpensesTotal = numeral(visibleExpensesTotal / 100).format('0,0.00');
 
   return (
     <div className="page-header">
       <div className="content-container">
-        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>€{formattedExpensesTotal}</span></h1>
+        <h1 className="page-header__title">Viewing <span>{visibleExpenseCount}</span> {visibleExpenseWord} totalling <span>€{formattedExpensesTotal}</span></h1>
+        {hiddenExpenseCount > 0 && <p className="page-header__sub-title">{hiddenExpenseCount} {hiddenExpenseWord} filtered out</p>}
         <div className="page-header__actions">
           <Link className="button" to="/create">Add Expense</Link>
         </div>
@@ -24,8 +26,9 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
 const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
   return {
-    expenseCount: visibleExpenses.length,
-    expensesTotal: selectExpensesTotal(visibleExpenses),
+    hiddenExpenseCount: state.expenses.length - visibleExpenses.length,
+    visibleExpenseCount: visibleExpenses.length,
+    visibleExpensesTotal: selectExpensesTotal(visibleExpenses),
   }
 };
 
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpensesSummary } from '../../components/ExpensesSummary';
-import expenses from '../fixtures/expenses';
 
 test('should render ExpensesSummary correctly', () => {
-  const wrapper = shallow(<ExpensesSummary expenseCount={2} expensesTotal={123123}/>);
+  const wrapper = shallow(<ExpensesSummary visibleExpenseCount={2} visibleExpensesTotal={123123}/>);
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should render ExpensesSummary correctly when no expenses', () => {
-  const wrapper = shallow(<ExpensesSummary expenseCount={0} />);
+  const wrapper = shallow(<ExpensesSummary visibleExpenseCount={0} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpensesSummary correctly when filtered out expenses', () => {
+  const wrapper = shallow(<ExpensesSummary hiddenExpenseCount={1} visibleExpenseCount={2} visibleExpensesTotal={123123}/>);
   expect(wrapper).toMatchSnapshot();
 });
